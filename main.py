4c600a11b33e13a7f19cc4f3dfcbe2699b24de29
WhiteGOM53 <47145666+WhiteGOM53@users.noreply.github.com>
2022-06-24 01:31:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import os
 from Music import music_cog
 
 bot = commands.Bot(command_prefix='.')
-
 bot.remove_command('help')
 
 bot.add_cog(music_cog(bot))
 
-bot.run("OTg5MDQ4NzI1Nzk4MjU2NjQz.G835TS.j972Bo43pjITxbzy7LHPTJJtPvHC5bvXwYiMko")
+access_token = os.environ["BOT_TOKEN"]
+bot.run(access_token)
